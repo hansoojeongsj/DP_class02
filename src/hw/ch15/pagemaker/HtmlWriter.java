4aab8a013a0e7a506a6824b58f38c60e2c8bc3d9
hansoojeongsj <tnwjd3758@duksung.ac.kr>
2023-06-04 23:40:27 +0900

    아마hw.ch15과제까지..완료

---
+++ src/hw/ch15/pagemaker/HtmlWriter.java
@@ -0,0 +1,50 @@
+package hw.ch15.pagemaker;
+
+import java.io.Writer;
+import java.io.IOException;
+
+public class HtmlWriter {
+    private Writer writer;
+
+    public HtmlWriter(Writer writer) {
+        this.writer = writer;
+    }
+
+    // 타이틀 출력 
+    public void title(String title) throws IOException {
+        //writer.write("<!DOCTYPE html>\n");
+        writer.write("<html>\n");
+        writer.write("<head>\n");
+        writer.write("<title>" + title + "</title>\n");
+        writer.write("</head>\n");
+        writer.write("<body>");
+        writer.write("\n");
+        writer.write("<h1> 한수정의 URL Page </h1>");
+        writer.write("\n");
+    }
+
+    // 단락 출력 
+    public void paragraph(String msg) throws IOException {
+        writer.write("<p>" + msg + "</p>");
+        writer.write("\n");
+    }
+
+    // 링크 출력 
+    public void link(String href, String caption) throws IOException {
+        paragraph("<a href=\"" + href + "\">" + caption + "</a>");
+    }
+
+    // 이메일 주소 출력 
+    public void mailto(String mailaddr, String username) throws IOException {
+        // link("mailto:" + mailaddr, username);
+        link(mailaddr, username);
+    }
+
+    // HTML 닫기 
+    public void close() throws IOException {
+        writer.write("</body>\n");
+        writer.write("</html>");
+        writer.write("\n");
+        writer.close();
+    }
+}
